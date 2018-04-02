a085c8231c46f8ba1c3ff7a529dbd57d4ef720a3
Jon <jonathanotano@icloud.com>
2018-04-02 14:48:00 -0400

    added validation to form

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-03-24T00:00:00&endtime=2018-03-24T23:00:00
 // Sun Apr 01 2018 12:00:00 GMT-0400 (EDT)
-
-document.getElementById('form').onsubmit = function(e){
-    e.preventDefault();
-}
+//
+// document.getElementById('form').onsubmit = function(e){
+//     e.preventDefault();
+// }
 
 var requestString = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
 var error = document.querySelector('.error');
@@ -33,7 +33,8 @@ var endDate = flatpickr((document.querySelector('#endDate')), {
 // ************
 
 var req = new XMLHttpRequest();
-req.onreadystatechange = function() {
+req.onreadystatechange = function(e) {
+    e.preventDefault();
     if (this.readyState == 4 && this.status == 200) {
       //console.log(this.readyState, this.status);
       var response_Paragraph = document.getElementById('response');
